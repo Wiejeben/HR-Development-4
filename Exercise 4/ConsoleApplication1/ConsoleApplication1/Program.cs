525dba378087afcf0dd83fd7843e35410e0c8bc6
Maarten de Graaf <maarten@webtraders.nl>
2016-06-01 14:15:39 +0200

    Exercise 4

---
+++ Exercise 4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOption<int> number = new Some<int>(5);
+
+            Console.WriteLine(number.Visit<string>(
+                () => "I am none...",
+                x => x.ToString()
+            ));
+
+            Console.ReadKey();
+        }
+
+        interface IOption<T>
+        {
+            U Visit<U>(Func<U> onNone, Func<T, U> onSome);
+        }
+
+        class Some<T> : IOption<T>
+        {
+            private T Value;
+
+            public Some(T value)
+            {
+                this.Value = value;
+            }
+
+            public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+            {
+                return onSome(this.Value);
+            }
+        }
+
+        class None<T> : IOption<T>
+        {
+            public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+            {
+                return onNone();
+            }
+        }
+    }
+}
