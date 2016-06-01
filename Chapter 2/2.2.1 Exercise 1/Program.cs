410a22b6ac804f291ebdd1eedeb92d1234a56d63
Maarten de Graaf <maarten@webtraders.nl>
2016-06-01 15:54:06 +0200

    Assignment 2, chapter 2

---
+++ Chapter 2/2.2.1 Exercise 1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._1_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NaturalNumbers numbers = new NaturalNumbers();
+            numbers.GetNext();
+            numbers.GetNext();
+            numbers.GetNext();
+            IOption<int> result = numbers.GetNext();
+
+            Console.WriteLine(result.Visit(
+                () => "Nothing",
+                x => "The number is " + x.ToString()
+            ));
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IOption<T>
+    {
+        U Visit<U>(Func<U> onNone, Func<T, U> onSome);
+    }
+
+    interface Iterator<T>
+    {
+        IOption<T> GetNext();
+        void Reset();
+    }
+
+    class Some<T> : IOption<T>
+    {
+        private T Value;
+
+        public Some(T value)
+        {
+            this.Value = value;
+        }
+
+        public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+        {
+            return onSome(this.Value);
+        }
+    }
+
+    class None<T> : IOption<T>
+    {
+        public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+        {
+            return onNone();
+        }
+    }
+
+    class IterableList<T> : Iterator<T>
+    {
+        private List<T> List;
+        public IterableList(List<T> list)
+        {
+            this.List = list;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOption<T> GetNext()
+        {
+            if (this.List.Count() <= 0)
+            {
+                return new None<T>();
+            }
+
+            List<T> tmp = this.List;
+            this.List.Remove(this.List.First());
+            return new Some<T>(tmp.First());
+        }
+    }
+
+    class NaturalNumbers : Iterator<int>
+    {
+        private int First = -1;
+        private int Current;
+
+        public NaturalNumbers()
+        {
+            this.Reset();
+        }
+        
+        public IOption<int> GetNext()
+        {
+            this.Current = (this.Current + 1);
+            return new Some<int>(Current);
+        }
+
+        public void Reset()
+        {
+            this.Current = this.First;
+        }
+    }
+}
---
+++ Chapter 2/Base/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface Option<T>
+    {
+
+    }
+
+    interface GolfIterator<T>
+    {
+        void First();
+        void Next();
+        bool IsDone();
+        T CurrentItem();
+    }
+
+    interface Iterator<T>
+    {
+        Option<T> GetNext();
+        void Reset();
+    }
+
+    interface TraditionalIterator<T>
+    {
+        bool MoveNext();
+        T Current();
+    }
+
+
+}
