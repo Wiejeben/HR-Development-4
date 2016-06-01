a37f8ed3a302441d2a4b3048aa7f179442fc845c
Maarten de Graaf <maarten@webtraders.nl>
2016-06-01 12:53:01 +0200

    Exercise 1

---
+++ Exercise 1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberVisitor visitor = new NumberVisitor();
+            MyInt myInt = new MyInt();
+
+            myInt.Visit(visitor);
+
+            Console.ReadKey();
+        }
+    }
+
+    interface INumber
+    {
+        void Visit(INumberVisitor visitor);
+    }
+
+    interface INumberVisitor
+    {
+        void OnMyInt(MyInt input);
+        void OnMyFloat(MyFloat input);
+    }
+
+    class NumberVisitor : INumberVisitor
+    {
+        public void OnMyFloat(MyFloat input)
+        {
+            Console.WriteLine("Found float");
+        }
+
+        public void OnMyInt(MyInt input)
+        {
+            Console.WriteLine("Found int");
+        }
+    }
+
+    class MyInt : INumber
+    {
+        public void Visit(INumberVisitor visitor)
+        {
+            visitor.OnMyInt(this);
+        }
+    }
+
+    class MyFloat : INumber
+    {
+        public void Visit(INumberVisitor visitor)
+        {
+            visitor.OnMyFloat(this);
+        }
+    }
+}
