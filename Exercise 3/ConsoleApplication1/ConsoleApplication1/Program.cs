a9faa6090cc54843708bc6465f72b82fb54b368c
Maarten de Graaf <maarten@webtraders.nl>
2016-06-01 13:56:07 +0200

    Exercise 3

---
+++ Exercise 3/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPrettyPrinterIOptionVisitor visitor = new IntPrettyPrinterIOptionVisitor();
+            IOption<int> number = new Some<int>(5);
+            IOption<int> number_2 = new None<int>();
+
+            Console.WriteLine(number.Visit(visitor));
+            Console.WriteLine(number_2.Visit(visitor));
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IOption<T>
+    {
+        U Visit<U>(IOptionVisitor<T, U> visitor);
+    }
+
+    interface IOptionVisitor<T, U>
+    {
+        U OnSome(T input);
+        U OnNone();
+    }
+
+    class Some<T> : IOption<T>
+    {
+        public T value;
+
+        public Some(T input)
+        {
+            this.value = input;
+        }
+
+        public U Visit<U>(IOptionVisitor<T, U> visitor)
+        {
+            return visitor.OnSome(this.value);
+        }
+    }
+
+    class None<T> : IOption<T>
+    {
+        public U Visit<U>(IOptionVisitor<T, U> visitor)
+        {
+            return visitor.OnNone();
+        }
+    }
+
+    class IntPrettyPrinterIOptionVisitor : IOptionVisitor<int, string>
+    {
+        public string OnNone()
+        {
+            return "I am nothing ...";
+        }
+
+        public string OnSome(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
